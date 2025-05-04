b0ba7a9bd1d105507ff3347800ffbbb227983644
Chris <zawsx@users.noreply.github.com>
2025-05-04 14:14:29 +0100

    sdfsdsdfv

---
+++ szpub.js
@@ -0,0 +1,11 @@
+import ghpages from 'gh-pages'
+ghpages.publish('dist', 
+
+('dist', {
+  branch: 'main',
+  {cname: 'tesa2.limecomms.gq'},
+  repo: 'https://github.com/zawszaws/projectv22.git'
+}, 
+
+
+function(err) {});
